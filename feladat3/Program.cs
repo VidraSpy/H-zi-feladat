8e5862f7a999c114e4b5dcaac98c77ee76e1132a
VidraSpy <terminatorroland@gmail.com>
2018-09-24 14:34:29 +0200

    elso feladat

--- feladat3/Program.cs
+++ feladat3/Program.cs
@@ -11,7 +11,23 @@ namespace feladat3
     {
         static void Main(string[] args)
         {
-
+            int n = 10;
+            int[] tomb = new int[n];
+            Console.Write("írjon be 10 számot! ");
+            for (int i = 0; i < n; i++)
+            {
+                tomb[i] = int.Parse(Console.ReadLine());
+            }
+            int maxin = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (tomb[maxin] < tomb[i])
+                {
+                    maxin = i;
+                }
+            }
+            Console.WriteLine("Legnagyobb beírt szám: " + tomb[maxin].ToString());
+            Console.ReadKey();
         }
     }
 }
